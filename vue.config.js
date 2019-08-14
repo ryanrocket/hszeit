0adfae6c584f1d0cf1e166ea07603b681b86bf10
ryanrocket <37909218+ryanrocket@users.noreply.github.com>
2019-08-14 11:18:36 -0400

    remove un-needed headers

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,7 @@ module.exports = {
     port: 9000,
     hotOnly: true,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-      'Accept-Youtube-API-Version': 'v3'
+      'Access-Control-Allow-Origin': '*'
     },
   },
 };
