5ea34ea1c0fdbab87f6678b45f798b15bbac4ed2
ryanrocket <ryan.wans@icloud.com>
2019-08-15 20:06:09 -0400

    Unspecified Changes by Makefile

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Dark from './views/Dark.vue'
 import 'vue-material/dist/theme/default.css'
 
 Vue.use(Router)
@@ -16,6 +17,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Home
+    },
+    {
+      path: '/dark',
+      name: 'dark',
+      component: Dark
     },
     {
       path: '*',
---
+++ src/views/Dark.vue
@@ -0,0 +1,20 @@
+<template>
+  <DMain title="Header Setter" useExtHeadset="true">
+
+  </DMain>
+</template>
+
+<style lang="sass" scoped>
+@import '../assets/darktheme.scss'
+</style>
+
+
+<script>
+import DMain from '../components/DMain';
+
+export default {
+  components: {
+    DMain,
+  },
+};
+</script>
